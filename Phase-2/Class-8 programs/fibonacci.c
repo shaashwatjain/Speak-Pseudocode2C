42396da8b16282f4ce3f721ea8fabbada07bcb57
RaghavAggarwal0310 <raghavaggarwal03.ra@gmail.com>
2021-12-10 18:01:15 +0530

    Changed variable name in fib.txt

--- Phase-2/Class-8 programs/fibonacci.c
+++ Phase-2/Class-8 programs/fibonacci.c
@@ -4,24 +4,24 @@ int main()
     int number;
     // initialize first and second terms
     int first = 0;
-    int value = 1;
+    int second = 1;
     // initialize the next term (3rd term)
-    int next = first + value;
+    int next = first + second;
     // get no. of terms from user
     printf("Enter the number of terms\n");
     scanf("%d", &number);
-    // print the first two terms a and value
+    // print the two terms first and second
     printf("Fibonacci Series\n");
     printf("%d\n", first);
-    printf("%d\n", value);
+    printf("%d\n", second);
 
     // print 3rd to nth terms
     for(int i = 3; i <= number; ++i)
     {
         printf("%d\n", next);
-        first = value;
-        value = next;
-        next = first + value;
+        first = second;
+        second = next;
+        next = first + second;
     }
 
     return 0;
